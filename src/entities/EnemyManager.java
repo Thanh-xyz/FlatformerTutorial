b7628e1c064d6f13c248d23bc280875a8a1d4620
Thanh-xyz <nguyenthanh24423@gmail.com>
2025-04-27 16:38:00 +0700

    add enemy animation

---
+++ src/entities/EnemyManager.java
@@ -0,0 +1,52 @@
+package entities;
+
+import gamestates.Playing;
+import utilz.LoadSave;
+
+import static utilz.Constants.EnemyConstants.*;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class EnemyManager {
+    private Playing playing;
+    private BufferedImage[][] crabbyArr;
+    private ArrayList<Crabby> crabbies;
+
+    public EnemyManager(Playing playing) {
+        this.playing = playing;
+        loadEnemyImgs();
+        addEnemies();
+    }
+
+    private void addEnemies() {
+        crabbies = LoadSave.GetCrabs();
+        System.out.println("Enemies added" +  crabbies.size());
+    }
+
+    public void update() {
+        for (Crabby crabby : crabbies) {
+            crabby.update();
+        }
+    }
+
+    public void draw(Graphics g, int xLvlOffset) {
+        drawCrabs(g, xLvlOffset);
+
+    }
+
+    private void drawCrabs(Graphics g, int xLvlOffset) {
+        for (Crabby c : crabbies)
+            g.drawImage(crabbyArr[c.getEnemyState()][c.getAniIndex()], (int) c.getHitbox().x - xLvlOffset, (int) c.getHitbox().y, CRABBY_WIDTH, CRABBY_HEIGHT, null);
+    }
+
+    private void loadEnemyImgs() {
+        crabbyArr = new BufferedImage[5][9];
+        BufferedImage temp = LoadSave.GetSpriteAtlas(LoadSave.CRABBY_SPRITE);
+        for (int i = 0; i < crabbyArr.length; i++)
+            for (int j = 0; j < crabbyArr[i].length; j++)
+                crabbyArr[i][j] = temp.getSubimage(j * CRABBY_WIDTH_DEFAULT, i * CRABBY_HEIGHT_DEFAULT, CRABBY_WIDTH_DEFAULT, CRABBY_HEIGHT_DEFAULT);
+    }
+}
--- src/utilz/LoadSave.java
+++ src/utilz/LoadSave.java
@@ -1,5 +1,6 @@
 package utilz;
 
+import entities.Crabby;
 import main.Game;
 
 import javax.imageio.ImageIO;
@@ -7,13 +8,16 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+
+import static utilz.Constants.EnemyConstants.CRABBY;
 
 public class LoadSave {
 
     public static final String PLAYER_ATLAS = "player_sprites.png";
     public static final String LEVEL_ATLAS = "outside_sprites.png";
 //    public static final String LEVEL_ONE_ATLAS = "level_one_data.png";
-    public static final String LEVEL_ONE_ATLAS = "level_one_data_long.png";
+    public static final String LEVEL_ONE_ATLAS = "level_one_data_long1.png";
     public static final String MENU_BUTTONS = "button_atlas.png";
     public static final String MENU_BACKGROUND = "menu_background.png";
     public static final String PAUSE_BACKGROUND = "pause_menu.png";
@@ -24,6 +28,7 @@ public class LoadSave {
     public static final String PLAYING_BG_IMG = "playing_bg_img.png";
     public static final String BIG_CLOUDS = "big_clouds.png";
     public static final String SMALL_CLOUDS = "small_clouds.png";
+    public static final String CRABBY_SPRITE = "crabby_sprite.png";
 
     public static BufferedImage GetSpriteAtlas(String fileName) {
         BufferedImage image = null;
@@ -42,6 +47,19 @@ public class LoadSave {
             }
         }
         return image;
+    }
+
+    public static ArrayList<Crabby> GetCrabs() {
+        BufferedImage img = GetSpriteAtlas(LEVEL_ONE_ATLAS);
+        ArrayList<Crabby> list = new ArrayList<>();
+        for (int i = 0; i < img.getHeight(); i++)
+            for (int j = 0; j < img.getWidth(); j++) {
+                Color color = new Color(img.getRGB(j, i));
+                int value = color.getGreen();
+                if (value == CRABBY)
+                    list.add(new Crabby(j * Game.TILES_SIZE, i * Game.TILES_SIZE));
+            }
+        return list;
     }
 
     //Lay du lieu ban do
